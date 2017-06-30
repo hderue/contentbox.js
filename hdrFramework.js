93da457d26b7d06cb4dbd6a07fb729bea64d4f2a
hderue <hobie_95@live.nl>
2017-06-30 09:24:43 +0200

    hdrSettings
    Het toevoegen van de default settings aan het framework. Ik laat deze settings global omdat deze binnen meerdere functies terug moeten komen.

--- hdrFramework.js
+++ hdrFramework.js
@@ -1,5 +1,14 @@
 var hdrContentbox = (function () {
   
-  
+  	var hrSettings = {
+		width:              50, //kies tussen 25 en 100 
+		opacity:            0.2, //kies een opacity tussen 0.0 en 1.0
+		speed:              500, // kies een snelheid voor de animatie
+		slideEffect:        'fromRight', // kies tussen fromLeft of fromRight
+		closePosition:      'closeRight', // kies tussen closeLeft en closeRight
+		background:         '#e3e3e3',
+		ShadingBox:         'ShadingBox'
+	};
+
 
 })();
